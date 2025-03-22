cf53b96c0e048392e5c979d32f342e82223b8a43
Mehdi Belkhelfa <midou8687@gmail.com>
2025-03-22 14:17:01 +0100

    add JWT configuration

--- user-service/src/main/java/com/sparky/user_service/config/JwtAuthenticationFilter.java
+++ user-service/src/main/java/com/sparky/user_service/config/JwtAuthenticationFilter.java
@@ -25,12 +25,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 
     /**
-     * Same contract as for {@code doFilter}, but guaranteed to be
-     * just invoked once per request within a single request thread.
-     * See {@link #shouldNotFilterAsyncDispatch()} for details.
-     * <p>Provides HttpServletRequest and HttpServletResponse arguments instead of the
-     * default ServletRequest and ServletResponse ones.
-     *
      * @param request
      * @param response
      * @param filterChain
